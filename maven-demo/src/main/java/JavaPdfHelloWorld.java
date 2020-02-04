d9fcd233f665b38974a505d7a16362df16d9fc6e
Shashank <shashank@lntinfotech.com>
2020-02-04 10:21:34 +0530

    Changes Saved

---
+++ maven-demo/src/main/java/JavaPdfHelloWorld.java
@@ -0,0 +1,29 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+ 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+ 
+public class JavaPdfHelloWorld
+{
+   public static void main(String[] args)
+   {
+      Document document = new Document();
+      try
+      {
+         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Hello.pdf"));
+         document.open();
+         document.add(new Paragraph(".....WELCOME TO HELL...."));
+         document.close();
+         writer.close();
+      } catch (DocumentException e)
+      {
+         e.printStackTrace();
+      } catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      }
+   }
+}
